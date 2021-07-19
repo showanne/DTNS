cd5b28898cee2d77b1b532d2dd58284d8d87d6e6
showanne <showanne.e@gmail.com>
2021-07-19 23:05:20 +0800

    carousel create

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import VueMaterial from 'vue-material'
 // import { MdButton, MdContent, MdTabs } from 'vue-material/dist/components'
 import 'vue-material/dist/vue-material.min.css'
 import 'vue-material/dist/theme/default.css'
+import VueCarousel from 'vue-carousel'
 
 import App from './App.vue'
 import './registerServiceWorker'
@@ -11,16 +12,20 @@ import router from './router'
 import store from './store'
 // style
 import './style/style.scss'
+import './style/all.scss'
+// vue-material variables
+// import './style/variables.scss'
 
 // material icon
-import MenuIcon from 'vue-material-design-icons/Menu.vue'
-Vue.component('menu-icon', MenuIcon)
+// import MenuIcon from 'vue-material-design-icons/Menu.vue'
+// Vue.component('menu-icon', MenuIcon)
 
 // VueMaterial UI
 Vue.use(VueMaterial)
 // Vue.use(MdButton)
 // Vue.use(MdContent)
 // Vue.use(MdTabs)
+Vue.use(VueCarousel)
 
 Vue.config.productionTip = false
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,66 @@
 <template>
   <div id="home">
-
+    <div class="md-layout">
+      <div class="md-layout-item md-size-66 mx-auto md-small-size-100">
+        <md-card>
+          <carousel
+            :per-page="1"
+            loop
+            :speed="700"
+            autoplay
+            :autoplay-timeout="5000"
+            :mouse-drag="false"
+            navigationEnabled
+            navigationNextLabel="<i class='material-icons'>keyboard_arrow_right</i>"
+            navigationPrevLabel="<i class='material-icons'>keyboard_arrow_left</i>"
+          >
+            <slide>
+              <div class="carousel-caption">
+                <h4>
+                  <md-icon>location_on</md-icon>
+                  Somewhere Beyond, United States
+                </h4>
+              </div>
+              <img :src="carousel1" alt="carousel1" />
+            </slide>
+            <slide>
+              <div class="carousel-caption">
+                <h4>
+                  <md-icon>location_on</md-icon>
+                  Yellowstone National Park, United States
+                </h4>
+              </div>
+              <img :src="carousel2" alt="carousel2" />
+            </slide>
+            <slide>
+              <div class="carousel-caption">
+                <h4>
+                  <md-icon>location_on</md-icon>
+                  Yellowstone National Park, United States
+                </h4>
+              </div>
+              <img :src="carousel3" alt="carousel3" />
+            </slide>
+          </carousel>
+        </md-card>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   name: 'Home',
-  components: {
+  components: {},
+  data () {
+    return {
+      carousel1: 'https://picsum.photos/1920/1080/?random=1',
+      carousel2: 'https://picsum.photos/1920/1080/?random=2',
+      carousel3: 'https://picsum.photos/1920/1080/?random=3'
+      // carousel1: require("@/assets/img/nature-2.jpg"),
+      // carousel2: require("@/assets/img/nature.jpg"),
+      // carousel3: require("@/assets/img/nature-3.jpg")
+    }
   }
 }
 </script>
