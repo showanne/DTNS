cb9847d5c4765edd658ec3fd8539272160ad307d
showanne <showanne.e@gmail.com>
2021-07-23 23:10:10 +0800

    新增分享牆卡片樣式

--- src/views/Collection.vue
+++ src/views/Collection.vue
@@ -1,91 +1,7 @@
 <template>
   <div id="collection" class="md-layout md-gutter md-alignment-center">
 
-    <div class="md-layout-item md-medium-size-33 md-small-size-50 md-xsmall-size-100">
-        <md-card md-with-hover>
-          <md-card-media-cover md-solid>
-            <md-card-media md-ratio="1:1">
-              <img src="https://picsum.photos/600/600/?random=1" alt="Skyscraper">
-            </md-card-media>
-
-            <md-card-area>
-              <md-card-header>
-                <span class="md-title">Solid background</span>
-                <span class="md-subhead">1/1 image</span>
-              </md-card-header>
-
-              <md-card-actions  md-alignment="space-between">
-              <md-button class="md-icon-button">
-                <md-icon :md-src="require('../assets/icon/icon-share.svg')"></md-icon>
-              </md-button>
-
-              <md-button class="md-icon-button">
-                <md-icon>event_note</md-icon>
-              </md-button>
-              <span>110.07.23</span>
-
-              <md-button class="md-icon-button">
-                <md-icon>volunteer_activism</md-icon>
-              </md-button>
-
-              <md-button class="md-icon-button">
-                <md-icon>thumb_up_alt</md-icon>
-              </md-button>
-              <span>59</span>
-
-              <md-button class="md-icon-button">
-                <md-icon>cloud_download</md-icon>
-              </md-button>
-
-            </md-card-actions>
-            </md-card-area>
-          </md-card-media-cover>
-        </md-card>
-    </div>
-
-    <div class="md-accent md-layout-item md-medium-size-33 md-small-size-50 md-xsmall-size-100">
-        <md-card md-with-hover>
-          <md-ripple>
-            <md-card-header>
-              <md-avatar>
-                <Avatar />
-              </md-avatar>
-              <div class="md-title">Accent color</div>
-              <div class="md-subhead">With hover and ripple effects</div>
-            </md-card-header>
-
-            <md-card-content>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio itaque ea, nostrum odio. Dolores, sed accusantium quasi non.
-            </md-card-content>
-
-            <md-card-actions  md-alignment="space-between">
-              <md-button class="md-icon-button">
-                <md-icon :md-src="require('../assets/icon/icon-postIt.svg')"></md-icon>
-              </md-button>
-
-              <md-button class="md-icon-button">
-                <md-icon>event_note</md-icon>
-              </md-button>
-              <span>110.07.23</span>
-
-              <md-button class="md-icon-button">
-                <md-icon>volunteer_activism</md-icon>
-              </md-button>
-
-              <md-button class="md-icon-button">
-                <md-icon>thumb_up_alt</md-icon>
-              </md-button>
-              <span>59</span>
-
-              <md-button class="md-icon-button">
-                <md-icon>cloud_download</md-icon>
-              </md-button>
-
-            </md-card-actions>
-          </md-ripple>
-        </md-card>
-    </div>
-
+    <!-- 只有圖片的版型 -->
     <div class="md-layout-item md-medium-size-33 md-small-size-50 md-xsmall-size-100">
         <md-card md-with-hover>
           <md-card-header>
@@ -93,17 +9,201 @@
               <Avatar />
             </md-avatar>
 
-            <div class="md-title">Title goes here</div>
-            <div class="md-subhead">Subtitle here</div>
+            <div class="md-title">只有圖片的版型</div>
+            <div class="md-subhead">Nick name</div>
           </md-card-header>
 
           <md-card-media>
             <img src="https://picsum.photos/300/300/?random=2" alt="People">
           </md-card-media>
 
+          <!-- <md-card-content>
+            Lorem ipsum.
+          </md-card-content> -->
+
+          <md-card-actions  md-alignment="space-between">
+              <md-button class="md-icon-button">
+                <md-icon :md-src="require('../assets/icon/icon-novel.svg')"></md-icon>
+              </md-button>
+
+              <md-button class="md-icon-button">
+                <md-icon>event_note</md-icon>
+              </md-button>
+              <span>110.07.23</span>
+
+              <md-button class="md-icon-button">
+                <md-icon>volunteer_activism</md-icon>
+              </md-button>
+
+              <md-button class="md-icon-button">
+                <md-icon>thumb_up_alt</md-icon>
+              </md-button>
+              <span>59</span>
+
+              <md-button class="md-icon-button">
+                <md-icon>cloud_download</md-icon>
+              </md-button>
+
+            </md-card-actions>
+        </md-card>
+    </div>
+
+    <!-- 圖片+文字描述的版型 -->
+    <div class="md-layout-item md-medium-size-33 md-small-size-50 md-xsmall-size-100">
+        <md-card md-with-hover>
+          <md-card-header>
+            <md-avatar>
+              <Avatar />
+            </md-avatar>
+
+            <div class="md-title">圖片+文字描述的版型</div>
+            <div class="md-subhead">Nick name</div>
+          </md-card-header>
+
+          <md-card-media>
+            <img src="https://picsum.photos/1920/1080/?random=2" alt="People">
+          </md-card-media>
+
           <md-card-content>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio itaque ea nostrum.
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
           </md-card-content>
+
+          <md-card-actions  md-alignment="space-between">
+              <md-button class="md-icon-button">
+                <md-icon :md-src="require('../assets/icon/icon-novel.svg')"></md-icon>
+              </md-button>
+
+              <md-button class="md-icon-button">
+                <md-icon>event_note</md-icon>
+              </md-button>
+              <span>110.07.23</span>
+
+              <md-button class="md-icon-button">
+                <md-icon>volunteer_activism</md-icon>
+              </md-button>
+
+              <md-button class="md-icon-button">
+                <md-icon>thumb_up_alt</md-icon>
+              </md-button>
+              <span>59</span>
+
+              <md-button class="md-icon-button">
+                <md-icon>cloud_download</md-icon>
+              </md-button>
+
+            </md-card-actions>
+        </md-card>
+    </div>
+
+    <!-- 只有文字(引用佳句)的版型 -->
+    <div class="md-layout-item md-medium-size-33 md-small-size-50 md-xsmall-size-100">
+        <md-card md-with-hover>
+          <md-card-header>
+            <md-avatar>
+              <Avatar />
+            </md-avatar>
+
+            <div class="md-title">只有文字(引用佳句)的版型</div>
+            <div class="md-subhead">Nick name</div>
+          </md-card-header>
+
+            <md-card-content class="blockquote">
+              <blockquote contenteditable="true">
+                  <p>Edit this content to add your own quote</p>
+              </blockquote>
+
+              <cite contenteditable="true">-- Write your own name here</cite>
+            </md-card-content>
+
+          <md-card-actions  md-alignment="space-between">
+              <md-button class="md-icon-button">
+                <md-icon :md-src="require('../assets/icon/icon-novel.svg')"></md-icon>
+              </md-button>
+
+              <md-button class="md-icon-button">
+                <md-icon>event_note</md-icon>
+              </md-button>
+              <span>110.07.23</span>
+
+              <md-button class="md-icon-button">
+                <md-icon>volunteer_activism</md-icon>
+              </md-button>
+
+              <md-button class="md-icon-button">
+                <md-icon>thumb_up_alt</md-icon>
+              </md-button>
+              <span>59</span>
+
+              <md-button class="md-icon-button">
+                <md-icon>cloud_download</md-icon>
+              </md-button>
+
+            </md-card-actions>
+        </md-card>
+    </div>
+
+    <!-- 只有文字(文章或句子)的版型 -->
+    <div class="md-layout-item md-medium-size-33 md-small-size-50 md-xsmall-size-100">
+        <md-card md-with-hover>
+          <md-card-header>
+            <md-avatar>
+              <Avatar />
+            </md-avatar>
+
+            <div class="md-title">只有文字(文章或句子)的版型</div>
+            <div class="md-subhead">Nick name</div>
+          </md-card-header>
+
+            <md-card-content>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium quod neque optio et delectus obcaecati quos suscipit asperiores laborum soluta illum, culpa reiciendis iure reprehenderit adipisci magni officia hic laudantium!
+            </md-card-content>
+
+          <md-card-actions  md-alignment="space-between">
+              <md-button class="md-icon-button">
+                <md-icon :md-src="require('../assets/icon/icon-novel.svg')"></md-icon>
+              </md-button>
+
+              <md-button class="md-icon-button">
+                <md-icon>event_note</md-icon>
+              </md-button>
+              <span>110.07.23</span>
+
+              <md-button class="md-icon-button">
+                <md-icon>volunteer_activism</md-icon>
+              </md-button>
+
+              <md-button class="md-icon-button">
+                <md-icon>thumb_up_alt</md-icon>
+              </md-button>
+              <span>59</span>
+
+              <md-button class="md-icon-button">
+                <md-icon>cloud_download</md-icon>
+              </md-button>
+
+            </md-card-actions>
+        </md-card>
+    </div>
+
+    <!-- 只有文字(待辦事項)的版型 -->
+    <div class="md-layout-item md-medium-size-33 md-small-size-50 md-xsmall-size-100">
+        <md-card md-with-hover>
+          <md-card-header>
+            <md-avatar>
+              <Avatar />
+            </md-avatar>
+
+            <div class="md-title">只有文字(待辦事項)的版型</div>
+            <div class="md-subhead">Nick name</div>
+          </md-card-header>
+
+            <md-card-content>
+              <ul>
+                <li>1</li>
+                <li>2</li>
+                <li>3</li>
+              </ul>
+            </md-card-content>
 
           <md-card-actions  md-alignment="space-between">
               <md-button class="md-icon-button">
