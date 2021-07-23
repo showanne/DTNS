24c251376cb09912d1c0bc58f91b104f46297669
showanne <showanne.e@gmail.com>
2021-07-24 01:19:17 +0800

    update table style

--- src/views/Manage.vue
+++ src/views/Manage.vue
@@ -2,6 +2,30 @@
   <div id="manage">
     <h1>This is an manage page</h1>
 
+    <!-- table -->
+    <md-table v-model="people" md-card @md-selected="onSelect">
+      <md-table-toolbar>
+        <h1 class="md-title">With auto select and alternate headers</h1>
+      </md-table-toolbar>
+
+      <md-table-toolbar slot="md-table-alternate-header" slot-scope="{ count }">
+        <div class="md-toolbar-section-start">{{ getAlternateLabel(count) }}</div>
+
+        <div class="md-toolbar-section-end">
+          <md-button class="md-icon-button">
+            <md-icon>delete</md-icon>
+          </md-button>
+        </div>
+      </md-table-toolbar>
+
+      <md-table-row slot="md-table-row" slot-scope="{ item }" :md-disabled="item.name.includes('Stave')" md-selectable="multiple" md-auto-select>
+        <md-table-cell md-label="Name" md-sort-by="name">{{ item.name }}</md-table-cell>
+        <md-table-cell md-label="Email" md-sort-by="email">{{ item.email }}</md-table-cell>
+        <md-table-cell md-label="Gender" md-sort-by="gender">{{ item.gender }}</md-table-cell>
+        <md-table-cell md-label="Job Title" md-sort-by="title">{{ item.title }}</md-table-cell>
+      </md-table-row>
+    </md-table>
+
     <!-- Edit Profile -->
     <md-card>
       <md-card-header class="md-secondary">
@@ -15,31 +39,31 @@
           <div class="md-layout-item md-small-size-100 md-size-33">
             <md-field>
               <label>User Name</label>
-              <md-input v-model="username" type="text"></md-input>
+              <md-input v-model="edit.username" type="text"></md-input>
             </md-field>
           </div>
           <div class="md-layout-item md-small-size-100 md-size-33">
             <md-field>
               <label>Email Address</label>
-              <md-input v-model="emailadress" type="email"></md-input>
+              <md-input v-model="edit.emailadress" type="email"></md-input>
             </md-field>
           </div>
           <div class="md-layout-item md-small-size-100 md-size-50">
             <md-field>
               <label>First Name</label>
-              <md-input v-model="firstname" type="text"></md-input>
+              <md-input v-model="edit.firstname" type="text"></md-input>
             </md-field>
           </div>
           <div class="md-layout-item md-small-size-100 md-size-50">
             <md-field>
               <label>Last Name</label>
-              <md-input v-model="lastname" type="text"></md-input>
+              <md-input v-model="edit.lastname" type="text"></md-input>
             </md-field>
           </div>
           <div class="md-layout-item md-size-100">
             <md-field maxlength="5">
               <label>About Me</label>
-              <md-textarea v-model="aboutme"></md-textarea>
+              <md-textarea v-model="edit.aboutme"></md-textarea>
             </md-field>
           </div>
           <div class="md-layout-item md-size-100 text-right">
@@ -56,17 +80,66 @@ export default {
   name: 'Manage',
   data () {
     return {
-      username: null,
-      disabled: null,
-      emailadress: null,
-      lastname: null,
-      firstname: null,
-      address: null,
-      city: null,
-      country: null,
-      code: null,
-      aboutme:
-        "Lamborghini Mercy, Your chick she so thirsty, I'm in that two seat Lambo."
+      edit: {
+        username: null,
+        disabled: null,
+        emailadress: null,
+        lastname: null,
+        firstname: null,
+        address: null,
+        city: null,
+        country: null,
+        code: null,
+        aboutme:
+          "Lamborghini Mercy, Your chick she so thirsty, I'm in that two seat Lambo."
+      },
+      selected: {},
+      people: [
+        {
+          name: 'Shawna Dubbin',
+          email: 'sdubbin0@geocities.com',
+          gender: 'Male',
+          title: 'Assistant Media Planner'
+        },
+        {
+          name: 'Odette Demageard',
+          email: 'odemageard1@spotify.com',
+          gender: 'Female',
+          title: 'Account Coordinator'
+        },
+        {
+          name: 'Lonnie Izkovitz',
+          email: 'lizkovitz3@youtu.be',
+          gender: 'Female',
+          title: 'Operator'
+        },
+        {
+          name: 'Thatcher Stave',
+          email: 'tstave4@reference.com',
+          gender: 'Male',
+          title: 'Software Test Engineer III'
+        },
+        {
+          name: 'Clarinda Marieton',
+          email: 'cmarietonh@theatlantic.com',
+          gender: 'Female',
+          title: 'Paralegal'
+        }
+      ]
+    }
+  },
+  methods: {
+    onSelect (items) {
+      this.selected = items
+    },
+    getAlternateLabel (count) {
+      let plural = ''
+
+      if (count > 1) {
+        plural = 's'
+      }
+
+      return `${count} user${plural} selected`
     }
   }
 }
