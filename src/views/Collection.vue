26186729b5ccc64c6e97584855c0c64e4041c974
showanne <showanne.e@gmail.com>
2021-07-26 21:10:51 +0800

    版面 scss 調整

--- src/views/Collection.vue
+++ src/views/Collection.vue
@@ -236,22 +236,56 @@
             </md-card-actions>
         </md-card>
     </div>
+
+        <!-- 只有圖片的版型 -->
+    <div class="md-layout-item md-medium-size-33 md-small-size-50 md-xsmall-size-100">
+        <md-card md-with-hover>
+          <md-card-header>
+            <md-avatar>
+              <Avatar />
+            </md-avatar>
+
+            <div class="md-title">只有圖片的版型</div>
+            <div class="md-subhead">Nick name</div>
+          </md-card-header>
+
+          <md-card-media>
+            <img src="https://picsum.photos/300/300/?random=2" alt="People">
+          </md-card-media>
+
+          <!-- <md-card-content>
+            Lorem ipsum.
+          </md-card-content> -->
+
+          <md-card-actions  md-alignment="space-between">
+              <md-button class="md-icon-button">
+                <md-icon :md-src="require('../assets/icon/icon-novel.svg')"></md-icon>
+              </md-button>
+
+              <md-button class="md-icon-button">
+                <md-icon>event_note</md-icon>
+              </md-button>
+              <span>110.07.23</span>
+
+              <md-button class="md-icon-button">
+                <md-icon>volunteer_activism</md-icon>
+              </md-button>
+
+              <md-button class="md-icon-button">
+                <md-icon>thumb_up_alt</md-icon>
+              </md-button>
+              <span>59</span>
+
+              <md-button class="md-icon-button">
+                <md-icon>cloud_download</md-icon>
+              </md-button>
+
+            </md-card-actions>
+        </md-card>
+    </div>
+
   </div>
 </template>
-
-<style lang="scss" scoped>
-  // @import "~vue-material/dist/theme/engine";;
-  // @include md-register-theme("orange-card", (
-  //   primary: md-get-palette-color(orange, 500)
-  // ));
-  // @import "~vue-material/base/theme";
-  // @import "~vue-material/components/MdCard/theme";
-
-  .md-layout-item {
-    margin-top: 8px;
-    margin-bottom: 8px;
-  }
-</style>
 
 <script>
 // components元件
