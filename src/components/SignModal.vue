0362791601cc6889432d559a531336d16181057f
showanne <showanne.e@gmail.com>
2021-07-26 21:10:06 +0800

    登入modal 調整

--- src/components/SignModal.vue
+++ src/components/SignModal.vue
@@ -1,114 +1,146 @@
 <template>
-  <md-dialog :md-active.sync="signBtn"
-  :md-close-on-esc="false"
-  :md-click-outside-to-close="false"
-  >
-    <md-dialog-title>
-      <div class="md-layout md-alignment-center-space-between">
-        <!-- <md-icon class="md-size-3x signIcon">person_pin</md-icon> -->
-        <slot name="title"></slot>
+  <div>
+    <md-dialog :md-active.sync="signBtn"
+    :md-close-on-esc="false"
+    :md-click-outside-to-close="false"
+    >
+      <md-dialog-title>
+        <div class="md-layout md-alignment-center-space-between">
+          <!-- <md-icon class="md-size-3x signIcon">person_pin</md-icon> -->
+          <slot name="title"></slot>
 
-        <md-button class="md-primary" @click="closeModal">
-          close &times;
-        </md-button>
-      </div>
-    </md-dialog-title>
-
-    <md-dialog-content class="md-scrollbar">
-      <md-tabs md-alignment="fixed">
-
-        <md-tab exact id="tab-line" md-label="line" :md-icon="require('../assets/icon/line-logo.svg')">
-
-          <md-button>
-            <md-icon :md-src="require('../assets/icon/line-logo.svg')"></md-icon>
+          <md-button class="md-primary" @click="closeModal">
+            close &times;
           </md-button>
-            Line 快速註冊
-        </md-tab>
+        </div>
+      </md-dialog-title>
 
-        <md-tab id="tab-manage" md-label="manage" md-icon="person">
-          <form
-            novalidate
-            class="md-layout"
-            @submit.prevent="validateUser"
-            @reset="clearForm"
-          >
-            <md-card class="md-layout-item" >
-              <md-card-content>
-                <!-- 帳號 Account -->
-                <md-field :class="getValidationClass('account')">
-                  <label for="account">Account</label>
-                  <md-input
-                    name="account"
-                    autocomplete="account"
-                    v-model="form.account"
-                    placeholder="You Nick Name..."
-                    :disabled="sending"
-                  />
-                  <span class="md-error" v-if="!$v.form.account.required"
-                    >帳號是必填欄位</span
-                  >
-                  <span class="md-error" v-else-if="!$v.form.account.minlength"
-                    >帳號最少須 5 個字，最多 20 個字</span
-                  >
-                </md-field>
+      <md-dialog-content class="md-scrollbar">
+        <md-tabs md-alignment="fixed">
 
-                <!-- 密碼 Password -->
-                <md-field :class="getValidationClass('password')">
-                  <label for="password">Password</label>
-                  <md-input
-                    type="password"
-                    name="password"
-                    autocomplete="password"
-                    v-model="form.password"
-                    placeholder="password..."
-                    :disabled="sending"
-                  />
-                  <span class="md-error" v-if="!$v.form.password.required"
-                    >密碼是必填欄位</span
-                  >
-                  <span class="md-error" v-else-if="!$v.form.password.minlength"
-                    >密碼最少須 4 個字，最多 20 個字</span
-                  >
-                </md-field>
+          <md-tab exact id="tab-line" md-label="line" :md-icon="require('../assets/icon/line-logo.svg')">
 
-                <!-- 信箱 Email -->
-                <!-- <md-field :class="getValidationClass('email')">
-                  <label for="email">Email</label>
-                  <md-input
-                    type="email"
-                    name="email"
-                    autocomplete="email"
-                    v-model="form.email"
-                    :disabled="sending"
-                  />
-                  <span class="md-error" v-if="!$v.form.email.required"
-                    >信箱是必填欄位</span
-                  >
-                  <span class="md-error" v-else-if="!$v.form.email.email"
-                    >信箱格式不符</span
-                  >
-                </md-field> -->
-              </md-card-content>
+            <md-button>
+              <div class="md-layout">
+                <div class="md-layout-item">
+                  <md-icon :md-src="require('../assets/icon/line-logo.svg')"></md-icon>
+                </div>
+                <div class="md-layout-item">&nbsp; 快速登入</div>
+              </div>
+            </md-button>
+          </md-tab>
 
-              <md-progress-bar md-mode="indeterminate" v-if="sending" />
-
-              <md-card-actions>
-                <md-button type="reset" class="md-primary"> Reset</md-button>
-                <md-button type="submit" class="md-primary" :disabled="sending">
-                  Create user</md-button>
-              </md-card-actions>
-            </md-card>
-
-            <md-snackbar :md-active.sync="userSaved"
-              >The user {{ user }} was saved with {{ message }}!</md-snackbar
+          <md-tab id="tab-manage" md-label="manage" md-icon="person">
+            <form
+              novalidate
+              class="md-layout"
+              @submit.prevent="validateUser"
+              @reset="clearForm"
             >
-          </form>
-        </md-tab>
-      </md-tabs>
-    </md-dialog-content>
+              <md-card class="md-layout-item" >
+                <md-card-content>
+                  <!-- 帳號 Account -->
+                  <md-field :class="getValidationClass('account')">
+                    <label for="account">Account</label>
+                    <md-input
+                      name="account"
+                      autocomplete="account"
+                      v-model="form.account"
+                      placeholder="You Nick Name..."
+                      :disabled="sending"
+                    />
+                    <span class="md-error" v-if="!$v.form.account.required"
+                      >帳號是必填欄位</span
+                    >
+                    <span class="md-error" v-else-if="!$v.form.account.minlength"
+                      >帳號最少須 5 個字，最多 20 個字</span
+                    >
+                  </md-field>
 
-  </md-dialog>
-  <!-- 參考 https://stackoverflow.com/questions/61611210/opening-and-closing-vue-material-dialog-on-different-components -->
+                  <!-- 密碼 Password -->
+                  <md-field :class="getValidationClass('password')">
+                    <label for="password">Password</label>
+                    <md-input
+                      type="password"
+                      name="password"
+                      autocomplete="password"
+                      v-model="form.password"
+                      placeholder="password..."
+                      :disabled="sending"
+                    />
+                    <span class="md-error" v-if="!$v.form.password.required"
+                      >密碼是必填欄位</span
+                    >
+                    <span class="md-error" v-else-if="!$v.form.password.minlength"
+                      >密碼最少須 4 個字，最多 20 個字</span
+                    >
+                  </md-field>
+
+                  <!-- 信箱 Email -->
+                  <!-- <md-field :class="getValidationClass('email')">
+                    <label for="email">Email</label>
+                    <md-input
+                      type="email"
+                      name="email"
+                      autocomplete="email"
+                      v-model="form.email"
+                      :disabled="sending"
+                    />
+                    <span class="md-error" v-if="!$v.form.email.required"
+                      >信箱是必填欄位</span
+                    >
+                    <span class="md-error" v-else-if="!$v.form.email.email"
+                      >信箱格式不符</span
+                    >
+                  </md-field> -->
+                </md-card-content>
+
+                <md-progress-bar md-mode="indeterminate" v-if="sending" />
+
+                <md-card-actions>
+                  <md-button type="reset" class="md-primary"> Reset</md-button>
+                  <md-button type="submit" class="md-primary" :disabled="sending">
+                    Sign In</md-button>
+                </md-card-actions>
+              </md-card>
+
+              <!-- Sign Up -->
+              <!-- <md-snackbar :md-active.sync="userSaved"
+                >The user {{ user }} was saved with {{ message }}!</md-snackbar> -->
+              <!-- Sign In -->
+              <md-snackbar :md-active.sync="userSaved"
+            >{{ message }}</md-snackbar>
+
+            <!-- <md-dialog-alert
+              :md-active.sync="userSaved"
+              md-title="ERROR"
+              :md-content="message" /> -->
+
+            </form>
+          </md-tab>
+        </md-tabs>
+      </md-dialog-content>
+
+    </md-dialog>
+    <!-- 參考 https://stackoverflow.com/questions/61611210/opening-and-closing-vue-material-dialog-on-different-components -->
+
+    <md-dialog-alert
+      :md-active.sync="userSaved"
+      md-title="ERROR"
+      :md-content="message" />
+    <!-- <md-dialog :md-active.sync="userSaved">
+      <md-dialog-content>
+        <md-button>
+          <div class="md-layou">
+            <div class="md-layout-item">
+              <md-icon :md-src="require('../assets/icon/icon-share.svg')"></md-icon>
+            </div>
+            <div class="md-layout-item">{{ message }}</div>
+          </div>
+        </md-button>
+      </md-dialog-content>
+    </md-dialog> -->
+  </div>
 </template>
 
 <script>
@@ -141,7 +173,7 @@ export default {
       userSaved: false,
       // 送出後的進度條 false 是不跑
       sending: false,
-      user: '',
+      // 登入訊息
       message: ''
     }
   },
@@ -175,39 +207,39 @@ export default {
     },
     clearForm () {
       this.$v.$reset()
+      this.sending = false
       this.form.account = ''
       this.form.password = ''
       // this.form.email = ''
     },
-    async saveUser () {
+    async signIn () {
       try {
-        await this.axios.post('/users', this.form)
+        const { data } = await this.axios.post('/users/signIn', this.form)
 
         this.sending = true
 
-        // Instead of this timeout, here you can call your API
-        // window.setTimeout(() => {
-        this.user = `${this.form.account}`
-        this.message = 'success'
-        this.userSaved = true
-        this.sending = false
-        this.clearForm()
-        // }, 1500)
+        window.setTimeout(() => {
+          this.message = `Wellcome ${this.form.account}`
+          this.userSaved = true
+          this.sending = false
+          // this.clearForm()
+
+          this.$store.commit('signIn', data)
+          // 登入成功後導向會員中心
+          this.$router.push('/member')
+        }, 1500)
       } catch (error) {
-        // window.setTimeout(() => {
-        this.user = `${this.form.account}`
-        this.userSaved = false
-        this.message = 'error'
+        this.userSaved = true
+        this.message = `${this.form.account}` + error
         this.sending = true
         // this.clearForm()
-        // }, 1500)
       }
     },
     validateUser () {
       this.$v.$touch()
 
       if (!this.$v.$invalid) {
-        this.saveUser()
+        this.signIn()
       }
     },
     closeModal () {
