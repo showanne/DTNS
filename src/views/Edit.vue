fa9efbc8f18eb3585a40c1a531571914658106e4
showanne <showanne.e@gmail.com>
2021-07-26 16:53:45 +0800

    調整編輯版面

--- src/views/Edit.vue
+++ src/views/Edit.vue
@@ -3,154 +3,165 @@
     <!-- <h1>This is an edit page</h1> -->
 
     <div class="md-layout md-gutter md-alignment-space-around">
-      <div id="temp" class="md-layout-item md-size-33 md-layout">
-        <!-- 有漸層背景及文字
-        <div class="md-layout-item md-size-50">
-          <md-card md-with-hover>
-            <md-ripple>
-              <md-card-media-cover md-text-scrim>
-                <md-card-media md-ratio="1:1">
-                  <img :src="require('../assets/icon/iconpostIt.svg')" alt="Skyscraper">
-                </md-card-media>
+      <div id="temp" class="md-layout-item md-large-size-33 md-medium-size-33 md-small-size-100">
+        <div class="md-layout">
+          <!-- 有漸層背景及文字
+            <div class="md-layout-item md-size-50 md-xsmall-size-15">
+              <md-card md-with-hover>
+                <md-ripple>
+                  <md-card-media-cover md-text-scrim>
+                    <md-card-media md-ratio="1:1">
+                      <img :src="require('../assets/icon/iconpostIt.svg')" alt="Skyscraper">
+                    </md-card-media>
 
-                 <md-card-area>
-                  <md-card-header>
-                    <span class="md-title">G</span>
-                    <span class="md-subhead">postIt</span>
-                  </md-card-header>
-                </md-card-area>
-              </md-card-media-cover>
-            </md-ripple>
-          </md-card>
-        </div> -->
+                    <md-card-area>
+                      <md-card-header>
+                        <span class="md-title">G</span>
+                        <span class="md-subhead">postIt</span>
+                      </md-card-header>
+                    </md-card-area>
+                  </md-card-media-cover>
+                </md-ripple>
+              </md-card>
+            </div> -->
 
-        <div class="md-layout-item md-size-50">
-          <md-card md-with-hover>
-            <md-ripple>
-              <md-card-media-cover>
-                <md-card-media md-ratio="1:1">
-                  <img :src="require('../assets/icon/iconpostIt.svg')" alt="Skyscraper">
-                </md-card-media>
+          <div class="md-layout-item md-size-50 md-xsmall-size-15">
+            <md-card md-with-hover>
+              <md-ripple>
+                <md-card-media-cover>
+                  <md-card-media md-ratio="1:1">
+                    <img
+                      :src="require('../assets/icon/iconpostIt.svg')"
+                      alt="Skyscraper"
+                    />
+                  </md-card-media>
 
-                <md-card-area>
-                  <md-card-header>
-                    <span>postIt</span>
-                  </md-card-header>
-                </md-card-area>
-              </md-card-media-cover>
-            </md-ripple>
-          </md-card>
+                  <md-card-area>
+                    <md-card-header>
+                      <span>postIt</span>
+                    </md-card-header>
+                  </md-card-area>
+                </md-card-media-cover>
+              </md-ripple>
+            </md-card>
+          </div>
+
+          <div class="md-layout-item md-size-50 md-xsmall-size-15">
+            <md-card md-with-hover>
+              <md-ripple>
+                <md-card-media-cover>
+                  <md-card-media md-ratio="1:1">
+                    <img
+                      :src="require('../assets/icon/iconshare.svg')"
+                      alt="Skyscraper"
+                    />
+                  </md-card-media>
+
+                  <md-card-area>
+                    <md-card-header>
+                      <span>share</span>
+                    </md-card-header>
+                  </md-card-area>
+                </md-card-media-cover>
+              </md-ripple>
+            </md-card>
+          </div>
+
+          <div class="md-layout-item md-size-50 md-xsmall-size-15">
+            <md-card md-with-hover>
+              <md-ripple>
+                <md-card-media-cover>
+                  <md-card-media md-ratio="1:1">
+                    <img
+                      :src="require('../assets/icon/iconnotes.svg')"
+                      alt="Skyscraper"
+                    />
+                  </md-card-media>
+
+                  <md-card-area>
+                    <md-card-header>
+                      <span>notes</span>
+                    </md-card-header>
+                  </md-card-area>
+                </md-card-media-cover>
+              </md-ripple>
+            </md-card>
+          </div>
         </div>
-
-        <div class="md-layout-item md-size-50">
-          <md-card md-with-hover>
-            <md-ripple>
-              <md-card-media-cover>
-                <md-card-media md-ratio="1:1">
-                  <img :src="require('../assets/icon/iconshare.svg')" alt="Skyscraper">
-                </md-card-media>
-
-                <md-card-area>
-                  <md-card-header>
-                    <span>share</span>
-                  </md-card-header>
-                </md-card-area>
-              </md-card-media-cover>
-            </md-ripple>
-          </md-card>
-        </div>
-
-        <div class="md-layout-item md-size-50">
-          <md-card md-with-hover>
-            <md-ripple>
-              <md-card-media-cover>
-                <md-card-media md-ratio="1:1">
-                  <img :src="require('../assets/icon/iconnotes.svg')" alt="Skyscraper">
-                </md-card-media>
-
-                <md-card-area>
-                  <md-card-header>
-                    <span>notes</span>
-                  </md-card-header>
-                </md-card-area>
-              </md-card-media-cover>
-            </md-ripple>
-          </md-card>
-        </div>
-
       </div>
 
-      <div class="md-layout-item md-size-66">
-        <form>
-          <md-field>
-            <label>Initial Value</label>
-            <md-input v-model="initial"></md-input>
-          </md-field>
+      <div id="tempEdit" class="md-layout-item md-large-size-66 md-medium-size-66 md-small-size-100">
+        <md-card>
+          <md-card-header>
+            <h4 class="title">Edit temp name</h4>
+            <!-- <p class="category">小標文字</p> -->
+          </md-card-header>
 
-          <md-field>
-            <label>Type here!</label>
-            <md-input v-model="type"></md-input>
-            <span class="md-helper-text">Helper text</span>
-          </md-field>
+          <md-card-content>
+            <form @submit.prevent="submitEdit" @reset="clearForm">
+              <div class="md-layout md-gutter">
+                <div class="md-layout-item md-small-size-100 md-size-66">
+                  <md-field>
+                    <label>Title</label>
+                    <md-input v-model="title"></md-input>
+                  </md-field>
+                </div>
+                <div class="md-layout-item md-small-size-100 md-size-33">
+                  <md-switch v-model="share" class="md-primary">share</md-switch>
+                </div>
 
-          <md-field>
-            <label>With label</label>
-            <md-input v-model="withLabel" placeholder="A nice placeholder"></md-input>
-          </md-field>
+                <div class="md-layout-item md-size-100">
+                  <md-field>
+                    <label>images</label>
+                    <md-file v-model="image" accept="image/*" />
+                  </md-field>
+                </div>
 
-          <md-field>
-            <label>Number</label>
-            <md-input v-model="number" type="number"></md-input>
-          </md-field>
+                <div class="md-layout-item md-size-100">
+                  <md-field>
+                    <label>Textarea</label>
+                    <md-textarea v-model="textarea"></md-textarea>
+                  </md-field>
+                </div>
 
-          <md-field>
-            <label>Textarea</label>
-            <md-textarea v-model="textarea"></md-textarea>
-          </md-field>
+                <div class="md-layout-item md-size-100">
+                  <md-field>
+                    <label>text</label>
+                    <md-input v-model="text"></md-input>
+                    <span class="md-helper-text">Helper text</span>
+                  </md-field>
+                </div>
 
-          <span>選單</span>
-          <md-field>
-            <md-select v-model="country" name="country" id="country" placeholder="Country">
-              <md-option value="australia">Australia</md-option>
-              <md-option value="brazil">Brazil</md-option>
-              <md-option value="japan">Japan</md-option>
-              <md-option value="united-states">United States</md-option>
-            </md-select>
-          </md-field>
+                <div class="md-layout-item md-size-100">
+                  <md-field>
+                    <md-select
+                      v-model="select"
+                      name="select"
+                      id="select"
+                      placeholder="select"
+                    >
+                      <md-option v-for="(Sitem, s) in select.finish"
+                      :value="Sitem" :key="s"
+                      >{{ Sitem }}</md-option>
+                    </md-select>
+                  </md-field>
+                </div>
 
-          <md-field>
-            <label for="movies">Movies</label>
-            <md-select v-model="selectedMovies" name="movies" id="movies" multiple>
-              <md-option value="fight-club">Fight Club</md-option>
-              <md-option value="godfather">Godfather</md-option>
-              <md-option value="godfather-ii">Godfather II</md-option>
-              <md-option value="godfather-iii">Godfather III</md-option>
-              <md-option value="godfellas">Godfellas</md-option>
-              <md-option value="pulp-fiction">Pulp Fiction</md-option>
-              <md-option value="scarface">Scarface</md-option>
-            </md-select>
-          </md-field>
+                <div class="md-layout-item md-size-100">
+                   <md-datepicker v-model="datepicker" md-immediately />
+                </div>
 
-          <span>檔案上傳</span>
-          <md-field>
-            <label>Single 單選</label>
-            <md-file v-model="single" />
-          </md-field>
+              </div>
 
-          <md-field>
-            <label>Multiple 多選</label>
-            <md-file v-model="multiple" multiple />
-          </md-field>
-
-          <md-field>
-            <label>Only images 只能傳圖片</label>
-            <md-file v-model="single" accept="image/*" />
-          </md-field>
-
-          <md-button class="md-raised" type="reset">
-            RESET
-          </md-button>
-        </form>
+            <md-button class="md-raised" type="reset">
+              RESET
+            </md-button>
+            <md-button class="md-raised" type="submit">
+              Submit
+            </md-button>
+            </form>
+          </md-card-content>
+        </md-card>
       </div>
     </div>
   </div>
@@ -161,15 +172,37 @@ export default {
   name: 'Edit',
   data () {
     return {
-      initial: 'text',
-      type: null,
-      withLabel: null,
-      number: null,
-      textarea: null,
-      country: null,
-      selectedMovies: [],
-      single: null,
-      multiple: null
+      temp: [
+        { name: '便利貼', subhead: 'postIt', src: '' },
+        { name: '美字美句分享', subhead: 'share', src: '' },
+        { name: '待辦事項', subhead: 'todo', src: '' },
+        { name: '心情隨筆', subhead: 'diary', src: '' },
+        { name: '筆記', subhead: 'notes', src: '' },
+        { name: '小說', subhead: 'novel', src: '' },
+        { name: '儲物清單', subhead: 'storage', src: '' }
+      ],
+      title: 'text',
+      share: true,
+      image: '',
+      textarea: '',
+      text: '',
+      select: {
+        mood: ['開心', '不好', '傷心'],
+        finish: ['很重要', '近期須完成', '普通']
+      },
+      datepicker: Number(new Date())
+    }
+  },
+  methods: {
+    submitEdit () {},
+    clearForm () {
+      this.title = ''
+      this.share = true
+      this.image = ''
+      this.textarea = ''
+      this.text = ''
+      this.select = ''
+      this.datepicker = ''
     }
   }
 }
