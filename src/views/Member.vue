49e976f0d25d8e7617ef5c0c0f825141dd4c20a3
showanne <showanne.e@gmail.com>
2021-08-05 01:19:05 +0800

    會員頁 版面

--- src/views/Member.vue
+++ src/views/Member.vue
@@ -1,13 +1,45 @@
 <template>
   <div id="member">
     <!-- <h1>This is an member page</h1> -->
-    <md-avatar v-if="user.avatar != null" class="md-large">
-      <img :src="user.avatar" alt="avatar">
-    </md-avatar>
-    <Avatar v-else />
-    <h2 v-if="user.name != null">{{ user.name }}</h2>
-    <h2 v-else-if="user.name == null && user.account != null">{{ user.account }}</h2>
-    <Anonymous v-else />
+    <div class="md-layout">
+      <div class="md-layout-item md-size-80">
+        member DATA TEMP ARTICLE
+      </div>
+      <div class="md-layout-item md-size-20">
+        <div class="full-control" style="border:solid 3px #ace; padding: 16px;">
+          <!-- 會員資訊 -->
+          <md-avatar v-if="user.avatar != null" class="md-large">
+            <img :src="user.avatar" alt="avatar">
+          </md-avatar>
+          <Avatar v-else />
+          <h2 v-if="user.name != null">{{ user.name }}</h2>
+          <h2 v-else-if="user.name == null && user.account != null">{{ user.account }}</h2>
+          <Anonymous v-else />
+
+          <!-- 會員可用功能選單 -->
+          <div class="list">
+            <md-list :md-expand-single="expandSingle">
+              <md-list-item md-expand :md-expanded.sync="expandNews">
+                <md-icon>whatshot</md-icon>
+                <span class="md-list-item-text">News</span>
+
+                <md-list slot="md-expand">
+                  <md-list-item class="md-inset">World</md-list-item>
+                  <md-list-item class="md-inset">Europe</md-list-item>
+                  <md-list-item class="md-inset">South America</md-list-item>
+                </md-list>
+              </md-list-item>
+
+              <md-list-item>
+                <md-icon>shopping_basket</md-icon>
+                <span class="md-list-item-text">Shop</span>
+              </md-list-item>
+            </md-list>
+          </div>
+        </div>
+      </div>
+    </div>
+
   </div>
 </template>
 
@@ -23,9 +55,8 @@ export default {
   name: 'Member',
   data () {
     return {
-      // query: {},
-      // token: {},
-      // idTokenDecode: {}
+      expandNews: false,
+      expandSingle: false
     }
   },
   components: {
