2767e0a5a2153f3cba2fbd4581cf4c7f7103ed2b
showanne <showanne.e@gmail.com>
2021-08-06 15:47:16 +0800

    安裝套件 vue-masonry 及 vue-easytable

--- src/main.js
+++ src/main.js
@@ -7,9 +7,16 @@ import 'vue-material/dist/vue-material.min.css'
 // axios
 import axios from 'axios'
 import VueAxios from 'vue-axios'
+// vue-carousel 輪播套件
 import VueCarousel from 'vue-carousel'
+// vue-masonry 瀑布流套件 ->解決 card 排版問題
+import { VueMasonryPlugin } from 'vue-masonry'
+// vue-img-inputer 圖片上傳套件
 import ImgInputer from 'vue-img-inputer'
 import 'vue-img-inputer/dist/index.css'
+// vue-easytable 表格套件
+import 'vue-easytable/libs/theme-default/index.css'
+import VueEasytable from 'vue-easytable'
 
 import App from './App.vue'
 import './registerServiceWorker'
@@ -26,16 +33,17 @@ import './style/style-custom.css'
 // 全域引用 mixin
 import mixin from './mixin.js'
 
-// VueMaterial UI
-Vue.use(VueMaterial)
-// Vue.use(MdButton)
-
 // 要注意有多個請求 api 不能寫 baseURL
 // 使用 baseURL 的方便之處在於 文件內有用到 axios 需要網址的地方可直接省略
 axios.defaults.baseURL = process.env.VUE_APP_API
+
+// VueMaterial UI
+Vue.use(VueMaterial)
 Vue.use(VueAxios, axios)
 Vue.use(VueCarousel)
+Vue.use(VueMasonryPlugin)
 Vue.component('ImgInputer', ImgInputer)
+Vue.use(VueEasytable)
 
 // 全域引用 mixin 方便各分頁直接用變數
 Vue.mixin(mixin)
