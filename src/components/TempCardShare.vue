cd6c10773844bfcf5dfe4a8ee7985499dcb73c58
showanne <showanne.e@gmail.com>
2021-08-06 23:11:33 +0800

    迴圈產生 md-tab 成功，文章顯示  TempCard 元件成功，分 temp 顯示研究失敗

---
+++ src/components/TempCardShare.vue
@@ -0,0 +1,92 @@
+<template>
+  <md-card md-with-hover>
+    <md-card-header>
+      <md-avatar class="md-large md-elevation-5">
+        <Avatar />
+      </md-avatar>
+
+      <div class="md-title">{{ item.title }}</div>
+      <div class="md-subhead">{{ item._id }}</div>
+      <div class="md-subhead">
+        <md-icon>event_note</md-icon>
+        {{ item.date }}
+      </div>
+    </md-card-header>
+
+    <md-card-media v-if="item.image">
+      <img :src="item.image" alt="People">
+    </md-card-media>
+
+    <md-card-content>
+      {{ item.textarea }}
+    </md-card-content>
+
+    <md-card-actions  md-alignment="space-between">
+      <md-button class="md-icon-button">
+        <md-icon :md-src="require('../assets/icon/temp-novel.svg')"></md-icon>
+      </md-button>
+
+      <md-speed-dial class="md-bottom-right" md-direction="top">
+        <md-speed-dial-target>
+          <md-icon class="md-morph-initial">add</md-icon>
+          <md-icon class="md-morph-final">edit</md-icon>
+        </md-speed-dial-target>
+
+        <md-speed-dial-content>
+
+          <md-button class="md-icon-button">
+            <md-icon>volunteer_activism</md-icon>
+          </md-button>
+
+          <md-button class="md-icon-button">
+            <md-icon>thumb_up_alt</md-icon>
+            <span>59</span>
+          </md-button>
+
+          <md-button class="md-icon-button">
+            <md-icon>cloud_download</md-icon>
+          </md-button>
+        </md-speed-dial-content>
+      </md-speed-dial>
+      <!-- <md-button class="md-icon-button">
+        <md-icon :md-src="require('../assets/icon/temp-novel.svg')"></md-icon>
+      </md-button>
+
+      <md-button class="md-icon-button">
+        <md-icon>event_note</md-icon>
+      </md-button>
+      <span>110.07.23</span>
+
+      <md-button class="md-icon-button">
+        <md-icon>volunteer_activism</md-icon>
+      </md-button>
+
+      <md-button class="md-icon-button">
+        <md-icon>thumb_up_alt</md-icon>
+      </md-button>
+      <span>59</span>
+
+      <md-button class="md-icon-button">
+        <md-icon>cloud_download</md-icon>
+      </md-button> -->
+
+    </md-card-actions>
+  </md-card>
+</template>
+
+<script>
+import Avatar from '@/components/Avatar.vue'
+
+export default {
+  name: 'TempCardShare',
+  components: {
+    Avatar
+  },
+  props: {
+    item: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
--- src/views/Collection.vue
+++ src/views/Collection.vue
@@ -1,344 +1,50 @@
 <template>
   <div id="collection" class="md-layout md-alignment-center">
     <div class="md-layout-item md-size-100">
-      <md-tabs  md-alignment="fixed">
-                <!-- v-for="(temp, T) in tempList"
-                :key="T" @click="tempShow(T)" -->
-        <md-tab id="tab-home" md-label="便利貼"
-                to="/collection/postIt"
-                :md-icon="require('../assets/icon/temp-postIt.svg')"></md-tab>
-        <md-tab id="tab-pages" md-label="美字美句分享"
-                to="/collection"
-                :md-icon="require('../assets/icon/temp-share.svg')"></md-tab>
-        <md-tab id="tab-pages1" md-label="待辦事項"
-                to="/collection/todo"
-                :md-icon="require('../assets/icon/temp-todo.svg')"></md-tab>
-        <md-tab id="tab-page2s" md-label="心情隨筆"
-                to="/collection/diary"
-                :md-icon="require('../assets/icon/temp-diary.svg')"></md-tab>
-        <md-tab id="tab-posts" md-label="筆記"
-                to="/collection/notes"
-                :md-icon="require('../assets/icon/temp-notes.svg')"></md-tab>
-        <md-tab id="tab-favorites" md-label="小說"
-                to="/collection/novel"
-                :md-icon="require('../assets/icon/temp-novel.svg')"></md-tab>
-        <md-tab id="tab-favorites" md-label="儲物清單"
-                to="/collection/storage"
-                :md-icon="require('../assets/icon/temp-storage.svg')"></md-tab>
+      <md-tabs md-alignment="fixed">
+        <md-tab v-for="(temp, T) in tempList"
+                :key="T" @click="tempShow(T)"
+                :id="'tab-'+temp.subhead"
+                :md-label="temp.name"
+                :md-icon="require('../assets/icon/temp-'+temp.subhead+'.svg')"
+                ></md-tab>
+                <!-- v-for="(temp, T) in tempList" :key="T" @click="tempShow(T) -->
+                <!-- :to="'/collection/'+temp.subhead" -->
+                <!-- v-if="+temp.show"  -->
       </md-tabs>
     </div>
     <div class="md-layout-item md-size-100">
       <transition>
-        <keep-alive>
-          <router-view></router-view>
-        </keep-alive>
+        <!-- <keep-alive> -->
+          <!-- <router-view> -->
+          <!-- </router-view> -->
+        <!-- </keep-alive> -->
       </transition>
+      <div
+        class="masonry-container"
+        v-masonry
+        transition-duration="0.3s"
+        item-selector=".masonry-container > .md-card"
+        fit-width="true"
+        v-for="item in article"
+        :key="item._id">
+        <TempCardShare v-if="item.template == tempCardShow" :item="item"></TempCardShare>
+        <!--  -->
+      </div>
     </div>
-
-    <div
-      class="masonry-container"
-      v-masonry
-      transition-duration="0.3s"
-      item-selector=".masonry-container > .md-card"
-      fit-width="true"
-    >
-      <!-- 只有圖片的版型 -->
-      <!-- <div class="md-layout-item md-medium-size-33 md-small-size-50 md-xsmall-size-100"> -->
-          <md-card md-with-hover>
-            <md-card-header>
-              <md-avatar class="md-large md-elevation-5">
-                <Avatar />
-              </md-avatar>
-
-              <div class="md-title">只有圖片的版型</div>
-              <div class="md-subhead">Nick name</div>
-            </md-card-header>
-
-            <md-card-media>
-              <img src="https://picsum.photos/300/300/?random=2" alt="People">
-            </md-card-media>
-
-            <!-- <md-card-content>
-              Lorem ipsum.
-            </md-card-content> -->
-
-            <md-card-actions  md-alignment="space-between">
-                <md-button class="md-icon-button">
-                  <md-icon :md-src="require('../assets/icon/temp-novel.svg')"></md-icon>
-                </md-button>
-
-                <md-button class="md-icon-button">
-                  <md-icon>event_note</md-icon>
-                </md-button>
-                <span>110.07.23</span>
-
-                <md-button class="md-icon-button">
-                  <md-icon>volunteer_activism</md-icon>
-                </md-button>
-
-                <md-button class="md-icon-button">
-                  <md-icon>thumb_up_alt</md-icon>
-                </md-button>
-                <span>59</span>
-
-                <md-button class="md-icon-button">
-                  <md-icon>cloud_download</md-icon>
-                </md-button>
-
-              </md-card-actions>
-          </md-card>
-      <!-- </div> -->
-
-      <!-- 圖片+文字描述的版型 -->
-      <!-- <div class="md-layout-item md-medium-size-33 md-small-size-50 md-xsmall-size-100"> -->
-          <md-card md-with-hover>
-            <md-card-header>
-              <md-avatar class="md-large md-elevation-5">
-                <Avatar />
-              </md-avatar>
-
-              <div class="md-title">圖片+文字描述的版型</div>
-              <div class="md-subhead">Nick name</div>
-            </md-card-header>
-
-            <md-card-media>
-              <img src="https://picsum.photos/1920/1080/?random=2" alt="People">
-            </md-card-media>
-
-            <md-card-content>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-            </md-card-content>
-
-            <md-card-actions  md-alignment="space-between">
-                <md-button class="md-icon-button">
-                  <md-icon :md-src="require('../assets/icon/temp-novel.svg')"></md-icon>
-                </md-button>
-
-                <md-button class="md-icon-button">
-                  <md-icon>event_note</md-icon>
-                </md-button>
-                <span>110.07.23</span>
-
-                <md-button class="md-icon-button">
-                  <md-icon>volunteer_activism</md-icon>
-                </md-button>
-
-                <md-button class="md-icon-button">
-                  <md-icon>thumb_up_alt</md-icon>
-                </md-button>
-                <span>59</span>
-
-                <md-button class="md-icon-button">
-                  <md-icon>cloud_download</md-icon>
-                </md-button>
-
-              </md-card-actions>
-          </md-card>
-      <!-- </div> -->
-
-      <!-- 只有文字(引用佳句)的版型 -->
-      <!-- <div class="md-layout-item md-medium-size-33 md-small-size-50 md-xsmall-size-100"> -->
-          <md-card md-with-hover>
-            <md-card-header>
-              <md-avatar class="md-large md-elevation-5">
-                <Avatar />
-              </md-avatar>
-
-              <div class="md-title">只有文字(引用佳句)的版型</div>
-              <div class="md-subhead">Nick name</div>
-            </md-card-header>
-
-              <md-card-content class="blockquote">
-                <figure>
-                  <blockquote>
-                      <p>Edit this content to add your own quote</p>
-                  </blockquote>
-
-                  <figcaption>––
-                    <span>作者 ngoewsghep</span>，
-                    <cite>
-                      <a href="">出處 Brave New World</a>
-                    </cite>
-                  </figcaption>
-                </figure>
-              </md-card-content>
-
-            <md-card-actions  md-alignment="space-between">
-                <md-button class="md-icon-button">
-                  <md-icon :md-src="require('../assets/icon/temp-novel.svg')"></md-icon>
-                </md-button>
-
-                <md-button class="md-icon-button">
-                  <md-icon>event_note</md-icon>
-                </md-button>
-                <span>110.07.23</span>
-
-                <md-button class="md-icon-button">
-                  <md-icon>volunteer_activism</md-icon>
-                </md-button>
-
-                <md-button class="md-icon-button">
-                  <md-icon>thumb_up_alt</md-icon>
-                </md-button>
-                <span>59</span>
-
-                <md-button class="md-icon-button">
-                  <md-icon>cloud_download</md-icon>
-                </md-button>
-
-              </md-card-actions>
-          </md-card>
-      <!-- </div> -->
-
-      <!-- 只有文字(文章或句子)的版型 -->
-      <!-- <div class="md-layout-item md-medium-size-33 md-small-size-50 md-xsmall-size-100"> -->
-          <md-card md-with-hover>
-            <md-card-header>
-              <md-avatar class="md-large md-elevation-5">
-                <Avatar />
-              </md-avatar>
-
-              <div class="md-title">只有文字(文章或句子)的版型</div>
-              <div class="md-subhead">Nick name</div>
-            </md-card-header>
-
-              <md-card-content>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium quod neque optio et delectus obcaecati quos suscipit asperiores laborum soluta illum, culpa reiciendis iure reprehenderit adipisci magni officia hic laudantium!
-              </md-card-content>
-
-            <md-card-actions  md-alignment="space-between">
-                <md-button class="md-icon-button">
-                  <md-icon :md-src="require('../assets/icon/temp-novel.svg')"></md-icon>
-                </md-button>
-
-                <md-button class="md-icon-button">
-                  <md-icon>event_note</md-icon>
-                </md-button>
-                <span>110.07.23</span>
-
-                <md-button class="md-icon-button">
-                  <md-icon>volunteer_activism</md-icon>
-                </md-button>
-
-                <md-button class="md-icon-button">
-                  <md-icon>thumb_up_alt</md-icon>
-                </md-button>
-                <span>59</span>
-
-                <md-button class="md-icon-button">
-                  <md-icon>cloud_download</md-icon>
-                </md-button>
-
-              </md-card-actions>
-          </md-card>
-      <!-- </div> -->
-
-      <!-- 只有文字(待辦事項)的版型 -->
-      <!-- <div class="md-layout-item md-medium-size-33 md-small-size-50 md-xsmall-size-100"> -->
-          <md-card md-with-hover>
-            <md-card-header>
-              <md-avatar class="md-large md-elevation-5">
-                <Avatar />
-              </md-avatar>
-
-              <div class="md-title">只有文字(待辦事項)的版型</div>
-              <div class="md-subhead">Nick name</div>
-            </md-card-header>
-
-              <md-card-content>
-                <ul>
-                  <li>1</li>
-                  <li>2</li>
-                  <li>3</li>
-                </ul>
-              </md-card-content>
-
-            <md-card-actions  md-alignment="space-between">
-                <md-button class="md-icon-button">
-                  <md-icon :md-src="require('../assets/icon/temp-novel.svg')"></md-icon>
-                </md-button>
-
-                <md-button class="md-icon-button">
-                  <md-icon>event_note</md-icon>
-                </md-button>
-                <span>110.07.23</span>
-
-                <md-button class="md-icon-button">
-                  <md-icon>volunteer_activism</md-icon>
-                </md-button>
-
-                <md-button class="md-icon-button">
-                  <md-icon>thumb_up_alt</md-icon>
-                </md-button>
-                <span>59</span>
-
-                <md-button class="md-icon-button">
-                  <md-icon>cloud_download</md-icon>
-                </md-button>
-
-              </md-card-actions>
-          </md-card>
-      <!-- </div> -->
-
-          <!-- 只有圖片的版型 -->
-      <!-- <div class="md-layout-item md-medium-size-33 md-small-size-50 md-xsmall-size-100"> -->
-          <md-card md-with-hover>
-            <md-card-header>
-              <md-avatar class="md-large md-elevation-5">
-                <Avatar />
-              </md-avatar>
-
-              <div class="md-title">只有圖片的版型</div>
-              <div class="md-subhead">Nick name</div>
-            </md-card-header>
-
-            <md-card-media>
-              <img src="https://picsum.photos/300/300/?random=2" alt="People">
-            </md-card-media>
-
-            <!-- <md-card-content>
-              Lorem ipsum.
-            </md-card-content> -->
-
-            <md-card-actions  md-alignment="space-between">
-                <md-button class="md-icon-button">
-                  <md-icon :md-src="require('../assets/icon/temp-novel.svg')"></md-icon>
-                </md-button>
-
-                <md-button class="md-icon-button">
-                  <md-icon>event_note</md-icon>
-                </md-button>
-                <span>110.07.23</span>
-
-                <md-button class="md-icon-button">
-                  <md-icon>volunteer_activism</md-icon>
-                </md-button>
-
-                <md-button class="md-icon-button">
-                  <md-icon>thumb_up_alt</md-icon>
-                </md-button>
-                <span>59</span>
-
-                <md-button class="md-icon-button">
-                  <md-icon>cloud_download</md-icon>
-                </md-button>
-
-              </md-card-actions>
-          </md-card>
-      <!-- </div> -->
-    </div>
-
   </div>
 </template>
 
 <script>
 // components元件
-import Avatar from '@/components/Avatar.vue'
+// import Avatar from '@/components/Avatar.vue'
+import TempCardShare from '@/components/TempCardShare.vue'
 
 export default {
   name: 'Collection',
   components: {
-    Avatar
+    // Avatar,
+    TempCardShare
   },
   data () {
     return {
@@ -350,13 +56,35 @@ export default {
         { show: this.$store.getters.user.isSignIn, name: '筆記', subhead: 'notes' },
         { show: this.$store.getters.user.isSignIn, name: '小說', subhead: 'novel' },
         { show: this.$store.getters.user.isSignIn, name: '儲物清單', subhead: 'storage' }
-      ]
+      ],
+      article: [],
+      tempCardShow: 0
     }
   },
   methods: {
     tempShow (T) {
       console.log(T)
-      // this.tempForm.template = T
+      this.tempCardShow = T
+    }
+  },
+  async mounted () {
+    try {
+      // 取得所有文章 /article
+      const { data } = await this.axios.get('/article')
+      this.article = data.result.map(article => {
+        // 有圖片才更新網址
+        if (article.image) {
+          // 處理 image 路徑  因為 :src 會錯誤判別 process.env  / 取得上傳的圖片 /file
+          article.image = `${process.env.VUE_APP_API}/file/${article.image}`
+        }
+        return article
+      })
+    } catch (error) {
+      console.log(error)
+      // let errorMsg = ''
+      // error.response.data.message === null ? errorMsg = error : errorMsg = error.response.data.message
+      // this.Msg = errorMsg
+      // this.msgModal = true
     }
   }
 }
