9cdaccea3a9aabad0d3a13a8f72ea715801fe0ff
showanne <showanne.e@gmail.com>
2021-08-15 00:56:14 +0800

    首頁版面微調

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,8 +3,8 @@
     <div class="md-layout">
       <div class="md-layout-item md-size-100">
         <div class="intro h-content">
-          <div class="md-display-3">文字的溫度</div>
-          <div class="md-display-2">每字美句每個你喜歡的瞬間</div>
+          <div class="md-display-3 lh-5 pl-5">文字的溫度</div>
+          <div class="md-display-1 pl-5">每字美句每個你喜歡的瞬間</div>
           <div class="md-layout">
             <div class="md-layout-item md-size-33">
               <md-card>
@@ -15,10 +15,10 @@
                 </md-card-media>
                 <md-card-header>
                   <div class="md-title">隨時記錄</div>
-                  <div class="md-subhead">Lorem ipsum dolor sit amet consectetur adi incidunt.</div>
+                  <div class="md-subhead">來自一本書、一句詞、一個讓你感動的瞬間。</div>
                 </md-card-header>
-                <md-card-actions>
-                  <md-button>一個置中的Action</md-button>
+                <md-card-actions class="flex-jc-medium-center">
+                  <md-button to="/edit">馬上開始...</md-button>
                 </md-card-actions>
               </md-card>
               <md-card>
@@ -36,10 +36,10 @@
                 </md-card-media>
                 <md-card-header>
                   <div class="md-title">方便儲存</div>
-                  <div class="md-subhead">Lorem ipsum dolor sit amet consectetur adi incidunt.</div>
+                  <div class="md-subhead">紀錄讓你心動的剎那，隨時回味翻找查詢。</div>
                 </md-card-header>
-                <md-card-actions>
-                  <md-button>一個置中的Action</md-button>
+                <md-card-actions class="flex-jc-medium-center">
+                  <md-button class="text-center" to="/member">立即註冊...</md-button>
                 </md-card-actions>
               </md-card>
               <md-card>
@@ -57,10 +57,10 @@
                 </md-card-media>
                 <md-card-header>
                   <div class="md-title">便捷分享</div>
-                  <div class="md-subhead">Lorem ipsum dolor sit amet consectetur adi incidunt.</div>
+                  <div class="md-subhead">立即跟親朋好友分享，創造更好的文字風景。</div>
                 </md-card-header>
-                <md-card-actions>
-                  <md-button>一個置中的Action</md-button>
+                <md-card-actions class="flex-jc-medium-center">
+                  <md-button class="text-center" to="/collection">來去看看...</md-button>
                 </md-card-actions>
               </md-card>
               <md-card>
